d8dc16299ae55132dfd9ee9a1af1526a84a5bbf7
moralerr <moralerrusc@gmail.com>
2024-07-05 15:55:16 -0400

    use single quotes for docker login

--- vars/buildNodeApp.groovy
+++ vars/buildNodeApp.groovy
@@ -41,7 +41,7 @@ def call(Map config = [:]) {
                         script {
                             def imageTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${config.branch}-${env.BUILD_NUMBER}"
                             withCredentials([usernamePassword(credentialsId: env.DOCKER_CREDENTIALS_ID, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                                sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+                                sh 'echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin'
                                 sh "docker push ${imageTag}"
                             }
                         }

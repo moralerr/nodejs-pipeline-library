8905bce5c3fad1981f35de2948b86fc75ec795c9
moralerr <moralerrusc@gmail.com>
2024-07-27 21:56:47 -0400

    replace template strings

--- vars/buildNodeApp.groovy
+++ vars/buildNodeApp.groovy
@@ -4,7 +4,7 @@ def call(Map config = [:]) {
     config = defaultConfig + config
     println config
     utils.logMapReadable(config)
-
+    utils.replaceTemplateStringsInYamlContent(config)
     pipeline {
         agent {
             kubernetes {

f5bdce4aa6e2fa8af1e4c37792fc7e7041157547
moralerr <16420343+moralerr@users.noreply.github.com>
2025-02-09 23:50:21 -0500

    Refactor Docker build and deployment process (#2)
    * use preview version of common library
    
    * test logMapReadable funct
    
    * move library definition
    
    * define library differently
    
    * add logging
    
    * use string interp for branch name
    
    * replace template strings
    
    * define build argument for source directory in Dockerfile
    
    * add default value for SOURCE_DIR in Dockerfile
    
    * refactor Dockerfile to streamline npm install commands and simplify variable usage
    
    * add source directory build argument to Docker build command in buildNodeApp.groovy
    
    * define build argument for source directory in Nginx stage of Dockerfile
    
    * add sourceDir configuration to defaultConfig.yml and update buildNodeApp.groovy to use new default path
    
    * add auto deployment stage to buildNodeApp.groovy for updating image tags in helm charts
    
    * secure git clone command in buildNodeApp.groovy using credentials
    
    * update release stage to overwrite existing values.yaml file during deployment
    
    * simplify branch checkout logic in buildNodeApp.groovy
    
    * update helm chart values.yaml tag handling to modify file content directly
    
    * refine tag update logic in buildNodeApp.groovy to target 'image.tag' line specifically
    
    * refactor tag update logic in buildNodeApp.groovy to use YAML parsing for values.yaml
    
    * set AUTO_DEPLOY_BRANCH to 'main' and update YAML write logic to allow overwriting
    
    * update buildNodeApp.groovy to correct branch name variable and restore Docker build/push stages
    
    * fix: correct branch name variable in image tag construction

--- vars/buildNodeApp.groovy
+++ vars/buildNodeApp.groovy
@@ -1,8 +1,10 @@
 def call(Map config = [:]) {
+    library('pipeline-commons@preview')
     def defaultConfig = readYaml(text: libraryResource('org/moralerr/config/defaultConfig.yml'))
     config = defaultConfig + config
     println config
-
+    utils.logMapReadable(config)
+    utils.replaceTemplateStringsInYamlContent(config)
     pipeline {
         agent {
             kubernetes {
@@ -12,13 +14,19 @@ def call(Map config = [:]) {
         environment {
             DOCKER_CREDENTIALS_ID = "${config.dockerCredentialsId}"
             DOCKER_REGISTRY_URL = "${config.dockerRegistryUrl}"
+            AUTO_DEPLOY_BRANCH = "main"
         }
         stages {
             stage('Copy Dockerfile') {
                 steps {
                     script {
+                        echo 'Checking if Dockerfile exists...'
                         if (!fileExists('Dockerfile')) {
+                            echo 'Dockerfile not found. Copying from library resources...'
                             writeFile file: 'Dockerfile', text: libraryResource('Dockerfile')
+                            echo 'Dockerfile successfully copied.'
+                        } else {
+                            echo 'Dockerfile already exists. No action taken.'
                         }
                     }
                 }
@@ -35,9 +43,10 @@ def call(Map config = [:]) {
                 steps {
                     container('dind') {
                         script {
-                            def imageTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${config.branch}-${env.BUILD_NUMBER}"
+                            def imageTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
                             def buildEnv = config.buildEnv ?: 'production'
-                            sh "docker build --build-arg BUILD_ENV=${buildEnv} -t ${imageTag} ."
+                            def sourceDir = config.sourceDir ?: '/app/dist'
+                            sh "docker build --build-arg BUILD_ENV=${buildEnv} --build-arg SOURCE_DIR=${sourceDir} -t ${imageTag} ."
                         }
                     }
                 }
@@ -46,7 +55,7 @@ def call(Map config = [:]) {
                 steps {
                     container('dind') {
                         script {
-                            def imageTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${config.branch}-${env.BUILD_NUMBER}"
+                            def imageTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
                             withCredentials([usernamePassword(credentialsId: env.DOCKER_CREDENTIALS_ID, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
                                 sh 'echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin'
                                 sh "docker push ${imageTag}"
@@ -67,6 +76,39 @@ def call(Map config = [:]) {
                             def releaseTag = "${config.dockerRegistryUrl}:${config.dockerImageName}-${env.GIT_TAG}"
                             sh "docker tag ${imageTag} ${releaseTag}"
                             sh "docker push ${releaseTag}"
+                        }
+                    }
+                }
+            }
+            stage('Auto Deploy') {
+                steps {
+                    script{
+                        sh 'rm -rf helm-charts'
+                        withCredentials([usernamePassword(credentialsId: 'GITHUB_ADMIN_TOKEN_AS_PASS',
+                                                             passwordVariable: 'GIT_PASSWORD',
+                                                             usernameVariable: 'GIT_USERNAME')]) {
+                            sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/moralerr/helm-charts.git helm-charts"
+                                                             }
+                        dir('helm-charts') {
+                            def valuesFile = "applications/${config.dockerImageName}/values.yaml"
+                            // Read YAML file content
+                            def valuesContent = readYaml(file: valuesFile)
+                            def newTag = "${config.dockerImageName}-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+                            // Update the 'tag' value within the 'image' section
+                            valuesContent.image.tag = newTag
+                            // Write updated content back to the YAML file
+                            writeYaml(file: valuesFile, data: valuesContent, overwrite: true)
+
+                            sh '''
+                                git config user.email "moralerrusc@gmail.com"
+                                git config user.name "Ricky"
+                            '''
+
+                            //sh 'git checkout -b test'
+
+                            sh "git add ${valuesFile}"
+                            sh "git commit -m 'Update image tag to ${newTag}'"
+                            sh "git push origin ${env.AUTO_DEPLOY_BRANCH}"
                         }
                     }
                 }
